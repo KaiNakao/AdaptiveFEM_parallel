b3ad289ac54cc9a14c3a44d8f0f93b589543fdcf
KaiNakao <106944655+KaiNakao@users.noreply.github.com>
2024-12-26 21:27:12 +0900

    add tools and jobscript

--- refiner/src/main.cpp
+++ refiner/src/main.cpp
@@ -1,16 +1,14 @@
-#include <string>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "inc/mesh_refiner.hpp"
 #include "inc/posterior_error.hpp"
 
-int main() 
-{
-    std::string data_dir("/data6/itou/AFEM/data/analysis_result_iburi_large/");
-    //ErrorEstimator error_estimator(data_dir);
-
+int main() {
+    std::string data_dir("result/");
+    ErrorEstimator error_estimator(data_dir);
 
     Refiner refiner = Refiner(data_dir);
     refiner.executeRefinement();
